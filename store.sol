dcbe810a3b6eed5a7fe48f8dd39492f9c23e4134
Pragmatic <cryptopragmatic@protonmail.com>
2023-12-09 19:20:04 +0100

    node token store

--- store.sol
+++ store.sol
@@ -6,17 +6,12 @@ contract AnoteStore {
 
     uint256 nodePrice;
 
-    function mintNode(uint256 amount) public payable {
-        require(msg.value == _withdrawFee * _withdrawCount[msg.sender], "Fee is required.");
-        
-        address payable owner = payable(owner());
-        owner.transfer(msg.value);
+    constructor() {
+        nodePrice = 50000000 gwei;
+    }
 
-        if (_withdraws[msg.sender] > 0) {
-            _mint(msg.sender, _withdraws[msg.sender]);
-            _withdraws[msg.sender] = 0;
-            _withdrawCount[msg.sender] = 0;
-        }
+    function mintNode(address addr, uint256 amount) public payable {
+        
     }
 
 }
