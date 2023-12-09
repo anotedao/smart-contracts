e7c641915eb8d238b376579d56a64dc02bc84b0c
Pragmatic <cryptopragmatic@protonmail.com>
2023-12-09 21:53:24 +0100

    finished node store first version

--- store.sol
+++ store.sol
@@ -2,16 +2,20 @@
 
 pragma solidity ^0.8.22;
 
-contract AnoteStore {
+contract AnoteNodeStore {
 
+    address private _owner;
     uint256 nodePrice;
+    uint8 nodeTier;
 
     constructor() {
         nodePrice = 50000000 gwei;
+        nodeTier = 10;
+        _owner = msg.sender;
     }
 
-    function mintNode(address addr, uint256 amount) public payable {
-        
+    function mintNode(string calldata addr) public payable {
+        payable(_owner).transfer(msg.value);
     }
 
 }
